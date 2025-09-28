2339007cec363d496043162c82ce25fffd723306
vannducs <nguyenvanducqlfptshop@gmail.com>
2025-09-28 08:02:55 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUsername = new TextBox();
+            lbUsername = new Label();
+            lbPassword = new Label();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(167, 55);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(222, 27);
+            txtUsername.TabIndex = 0;
+            txtUsername.TextChanged += textBox1_TextChanged;
+            // 
+            // lbUsername
+            // 
+            lbUsername.AutoSize = true;
+            lbUsername.Location = new Point(54, 58);
+            lbUsername.Name = "lbUsername";
+            lbUsername.Size = new Size(107, 20);
+            lbUsername.TabIndex = 1;
+            lbUsername.Text = "Tên đăng nhập";
+            // 
+            // lbPassword
+            // 
+            lbPassword.AutoSize = true;
+            lbPassword.Location = new Point(54, 116);
+            lbPassword.Name = "lbPassword";
+            lbPassword.Size = new Size(70, 20);
+            lbPassword.TabIndex = 3;
+            lbPassword.Text = "Mật khẩu";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(167, 113);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(222, 27);
+            txtPassword.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(210, 167);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Đăng nhập";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLogin);
+            Controls.Add(lbPassword);
+            Controls.Add(txtPassword);
+            Controls.Add(lbUsername);
+            Controls.Add(txtUsername);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUsername;
+        private Label lbUsername;
+        private Label lbPassword;
+        private TextBox txtPassword;
+        private Button btnLogin;
+    }
+}
